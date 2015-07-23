bd3405fd9ad377ed9727c8a78a5ba91cb6b44976
paladox <thomasmulhall410@yahoo.com>
2015-07-23 16:50:12 +0300

    Add composer/npm entry points
    Support for running npm tests
    Composer for doing php code sniffing.
    Update .gitignore
    
    Change-Id: I12a687acd9542621e6eabc81bae4275a35869f91

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+/*jshint node:true */
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-jscs' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+
+	grunt.initConfig( {
+		jshint: {
+			all: [
+				'*.js'
+			]
+		},
+		jscs: {
+			src: '<%= jshint.all %>'
+		},
+		banana: {
+			all: 'i18n/'
+		},
+		jsonlint: {
+			all: [
+				'*.json',
+				'**/*.json',
+				'!node_modules/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'jshint', 'jscs', 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'default', 'test' );
+};
