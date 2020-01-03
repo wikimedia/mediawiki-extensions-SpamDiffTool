0ac86cd8d00ce76c04a73617cf892af2706bc09d
DannyS712 <DannyS712.enwiki@gmail.com>
2020-01-03 06:14:25 +0000

    Remove commented out unused code from hooks file
    Shows up as a use of $wgUser, but the usage is commented out and has
    been for a while.
    
    Change-Id: I2ffff9eae17ec1b1e598b163d676a3c452810a61

--- SpamDiffToolHooks.php
+++ SpamDiffToolHooks.php
@@ -54,33 +54,4 @@ class SpamDiffToolHooks {
 
 		return true;
 	}
-
-	/** This is original-ish ashley code which looks ugly; the above version (by iAlex) is so much better.
-	public static function onDiffRevisionTools( Revision $newRev, &$revisionTools, $oldRev ) {
-		global $wgUser;
-		$t = $newRev->getTitle();
-		if ( $wgUser->isAllowed( 'rollback' ) && $t instanceof Title && $t->userCan( 'edit' ) ) {
-			$spamLink = '<br />&nbsp;&nbsp;&nbsp;<strong>' .
-				SpamDiffTool::getDiffLink( $t ) . '</strong>';
-		}
-		$revisionTools['mw-diff-spamdifftool'] = $spamLink;
-		return true;
-	}
-	**/
-
-	/*
-	public static function onArticleViewFooter( $article, $patrolFooterShown ) {
-		if ( $patrolFooterShown ) {
-			// @todo FIXME: this would need to go *inside* the div.patrollink div,
-			// but Article::showPatrolFooter() (=our $patrolFooterShown variable)
-			// appends to OutputPage via ->addHTML() instead of returning a string,
-			// so we can't do nifty str_replace() voodoo here and expect the code to
-			// perform well...
-			//$article->getContext()->getOutput(
-			SpamDiffTool::getDiffLink( $article->getTitle() );
-			//);
-		}
-		return true;
-	}
-	*/
 }
