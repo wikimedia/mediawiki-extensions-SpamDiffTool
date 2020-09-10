c58f2756ea6d2a8a657b90374a3268c1443235e1
zoranzoki21 <zorandori4444@gmail.com>
2020-09-10 20:00:57 +0200

    Add grunt-stylelint so css files can be tested also
    Change-Id: I3e7c9ac012b1fca0bb48c4490eb030f94e335c88

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@
 module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-eslint' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	grunt.initConfig( {
 		banana: {
@@ -17,9 +18,16 @@ module.exports = function ( grunt ) {
 				'!node_modules/**',
 				'!vendor/**'
 			]
+		},
+		stylelint: {
+			all: [
+				'**/*.css',
+				'!node_modules/**',
+				'!vendor/**'
+			]
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'eslint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'eslint', 'stylelint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
